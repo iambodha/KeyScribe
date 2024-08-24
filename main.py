8ea53baf6d0ea18f01e4e10313156c07c9dff0d9
iambodha <165776420+iambodha@users.noreply.github.com>
2024-08-24 12:15:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 from pynput import keyboard
+import time
 
-def on_press(key):
-    try:
-        print(f'Key pressed: {key.char}')
-    except AttributeError:
-        print(f'Special key pressed: {key}')
+class KeyLogger:
+    def __init__(self):
+        self.last_time = None
 
-def on_release(key):
-    if key == keyboard.Key.esc:
-        return False
+    def on_press(self, key):
+        current_time = time.time()
 
-with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-    listener.join()
+        if self.last_time is not None:
+            time_interval = current_time - self.last_time
+            print(f'Time since last press: {time_interval:.4f} seconds')
+        else:
+            print("First key press!")
+
+        try:
+            print(f'Key pressed: {key.char}')
+        except AttributeError:
+            print(f'Special key pressed: {key}')
+
+        self.last_time = current_time
+
+    def on_release(self, key):
+        if key == keyboard.Key.esc:
+            print("Exiting...")
+            return False
+
+    def start_logging(self):
+        with keyboard.Listener(on_press=self.on_press, on_release=self.on_release) as listener:
+            listener.join()
+
+if __name__ == "__main__":
+    key_logger = KeyLogger()
+    key_logger.start_logging()
